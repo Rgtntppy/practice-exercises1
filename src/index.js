6f704a866c30129c4c2769e950bf34980d11fadf
NULL <rgtntppy@gmail.com>
2023-08-14 09:26:04 +0900

    １行目追加

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import React from 'react';
+
 function Gif({ id }) {
   const src = `https://media.giphy.com/media/${id}/giphy.gif`;
   return <img src={src} alt="" />;
--- src/transpiled_index.js
+++ src/transpiled_index.js
@@ -1,14 +1,16 @@
 "use strict";
 
+var _react = _interopRequireDefault(require("react"));
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 function Gif(_ref) {
   var id = _ref.id;
   var src = "https://media.giphy.com/media/".concat(id, "/giphy.gif");
-  return /*#__PURE__*/React.createElement("img", {
+  return /*#__PURE__*/_react.default.createElement("img", {
     src: src,
     alt: ""
   });
 }
-var app = /*#__PURE__*/React.createElement(Gif, {
+var app = /*#__PURE__*/_react.default.createElement(Gif, {
   id: "33OrjzUFwkwEg"
 });
 var root = document.getElementById('root');
