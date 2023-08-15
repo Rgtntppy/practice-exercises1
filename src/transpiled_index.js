9e645664a8085ec04c2812b3d694efe873f27137
NULL <rgtntppy@gmail.com>
2023-08-15 15:52:20 +0900

    reactDOM追加

--- src/transpiled_index.js
+++ src/transpiled_index.js
@@ -1,6 +1,7 @@
 "use strict";
 
 var _react = _interopRequireDefault(require("react"));
+var _reactDom = _interopRequireDefault(require("react-dom"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 function Gif(_ref) {
   var id = _ref.id;
@@ -14,4 +15,4 @@ var app = /*#__PURE__*/_react.default.createElement(Gif, {
   id: "33OrjzUFwkwEg"
 });
 var root = document.getElementById('root');
-ReactDOM.render(app, root);
+_reactDom.default.render(app, root);
